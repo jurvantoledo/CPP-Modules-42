883d86425255cae138b759a2ef3e11535f9b941d
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-01-19 17:41:24 +0100

    finished ex05

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -5,22 +5,22 @@ int main()
     Harl lol;
 
     std::cout << std::endl;
-    std::cout << "DEBUG MESSAGE:" << std::endl;
+    std::cout << "[ DEBUG MESSAGE] :" << std::endl;
     lol.complain("DEBUG");
     std::cout << std::endl;
 
     std::cout << std::endl;
-    std::cout << "WARNING MESSAGE:" << std::endl;
+    std::cout << "[ WARNING MESSAGE] :" << std::endl;
     lol.complain("WARNING");
     std::cout << std::endl;
 
     std::cout << std::endl;
-    std::cout << "INFO MESSAGE:" << std::endl;
+    std::cout << "[ INFO MESSAGE] :" << std::endl;
     lol.complain("INFO");
     std::cout << std::endl;
 
     std::cout << std::endl;
-    std::cout << "ERROR MESSAGE:" << std::endl;
+    std::cout << "[ ERROR MESSAGE ]:" << std::endl;
     lol.complain("ERROR");
     std::cout << std::endl;
     return 0;
