94d3205bacc94521052882899d97badac5ba0934
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-01-19 17:14:20 +0100

    finished ex05

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -7,3 +7,58 @@ Harl::Harl(/* args */)
 Harl::~Harl()
 {
 }
+
+void Harl::debug()
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+    std::cout << "This is an INFO message" << std::endl;
+}
+
+void Harl::warning()
+{
+    std::cout << "This is an WARNING message" << std::endl;
+
+}
+
+void Harl::error()
+{
+    std::cout << "This is an ERROR message" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    // Array met strings
+    std::string levelArr[] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
+    };
+
+    // Pointer function met alle funties
+    // Maakt een pointer functie aan en in die pointer functie gooi ik andere functies.
+	typedef void(Harl::*ptrFunc[])();
+	ptrFunc func = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+    // loop om langs alle levels te gaan
+    int i = 0;
+    while (i < 4)
+    {
+        if (level == levelArr[i])
+        {
+            (this->*func[i])();
+            return ;
+        }
+        i++;
+    }
+    std::cout << "Not able to complain" << std::endl;
+    return ;
+}
