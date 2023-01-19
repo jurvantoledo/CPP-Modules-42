af5b4b2bdd3e96ac948824d947087930e22cbfb4
jurvantoledo <jurvantoledo@live.nl>
2023-01-19 10:54:09 +0100

    finished ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+
 int main()
 {
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Memory address of String: " << &str << std::endl;
+    std::cout << "Memory address of Pointer: " << stringPTR << std::endl;
+    std::cout << "Memory address of Reference: " << &stringREF << std::endl;
+
+    std::cout << "Value of String: " << str << std::endl;
+    std::cout << "Value of Pointer: " << *stringPTR << std::endl;
+    std::cout << "Value of Reference: " << stringREF << std::endl;
     return 0;
 }
