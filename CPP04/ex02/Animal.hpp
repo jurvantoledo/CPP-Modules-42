3025a32f41eb2157e708853e69ad001b044d221f
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-01-26 14:25:54 +0100

    finished ex02

--- CPP04/ex02/Animal.hpp
+++ CPP04/ex02/Animal.hpp
@@ -3,21 +3,30 @@
 #define ANIMAL_H
 #include <iostream>
 
+/**
+* @brief
+* So we made the class abstract which means its no longer instantiable. but it 
+* can be used as a base class.
+* = 0 is known as pure-specifier, and appears either immediately after 
+* the declarator or after the optional virt-specifier.
+* 
+* @brief A class is abstract if it has at least one pure virtual member function.
+*/
 class Animal
 {
     public:
         Animal();
         Animal(const Animal &obj);
         Animal &operator = (const Animal &obj);
-        virtual ~Animal();
+        virtual ~Animal() = 0;
 
         /**
-         * @brief 
+         * @brief
          * Basically, a virtual function is used in the base class in order to ensure that the function is overridden. 
          * This especially applies to cases where a pointer of base class points to an object of a derived class.
          * 
          */
-        virtual void makeSound(void) const;
+        virtual void makeSound(void) const = 0;
         std::string getType(void) const;
     
     protected:
--- CPP04/ex02/main.cpp
+++ CPP04/ex02/main.cpp
@@ -7,5 +7,40 @@
 
 int main()
 {
+    Animal *AnimalArray[10];
+
+    // 5 Dogs in array
+    for (int i = 0; i < 5; i++)
+    {
+        AnimalArray[i] = new Dog();
+    }
+
+    // 5 Cats in array
+    for (int i = 5; i < 10; i++)
+    {
+        AnimalArray[i] = new Cat();
+    }
+    
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "Dog and cat noises" << std::endl;
+    std::cout << "---------------------------------------------" << std::endl;
+
+    AnimalArray[1]->makeSound();
+    AnimalArray[9]->makeSound();
+
+    std::cout << "---------------------------------------------" << std::endl;
+
+    // delete full array
+    for (int i = 0; i < 10; i++)
+    {
+       delete AnimalArray[i];
+    }
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "---------------------------------------------" << std::endl;
+
+
+    // Onderstaande code is dus niet meer mogelijk omdat door virtual = 0 de class abstract is geworden
+    // const Animal* meta = new Animal();
+    
     return (0);
 }
