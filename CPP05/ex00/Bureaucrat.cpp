c95579b193be2f62ea85dcb33d9b59d0ae1d8a0c
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-01-26 15:13:20 +0100

    made start with ex00

---
+++ CPP05/ex00/Bureaucrat.cpp
@@ -0,0 +1,11 @@
+#include "Bureaucrat.hpp"
+
+Bureacucrat::Bureacucrat()
+{
+    std::cout << "Default Constructor is called" << std::endl;
+}
+
+Bureacucrat::~Bureacucrat()
+{
+    std::cout << "Destructor is called" << std::endl;
+}
