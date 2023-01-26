24e403784a40d12664757beef97a8d49abf9042e
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-01-26 14:38:11 +0100

    made start with ex00

---
+++ CPP05/ex01/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+#ifndef BUREAUCRAT_H
+
+#define BUREAUCRAT_H
+
+#include <iostream>
+
+class Bureacucrat
+{
+    public:
+        Bureacucrat(/* args */);
+        ~Bureacucrat();
+
+    private:
+};
+
+#endif
