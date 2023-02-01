efc68d2671d09e3102685de8132f5efe23b29566
Jur Van toledo <jvan-tol@f1r6s16.codam.nl>
2023-02-01 15:40:16 +0100

    started with ex02

---
+++ CPP05/ex02/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+#ifndef BUREAUCRAT_H
+
+#define BUREAUCRAT_H
+
+#include <iostream>
+
+class Bureaucrat
+{
+    public:
+        Bureaucrat(const std::string &input_name, int grade);
+        Bureaucrat(const Bureaucrat &obj);
+        Bureaucrat &operator=(const Bureaucrat &obj);
+        ~Bureaucrat();
+
+        // Member functions
+        std::string getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+
+        // Exception functions
+        class GradeTooHighException : std::exception
+		{
+			public:
+				const char *what() const throw ();
+				//{
+				//	return "Grade is to low";
+				//}
+		};
+        class GradeTooLowException : std::exception
+		{
+			public:
+				const char *what() const throw ();
+				//{
+				//	return "Grade is to low";
+				//}
+		};
+
+    private:
+        const std::string name;
+        int grade;
+};
+
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& bureau);
+
+#endif
