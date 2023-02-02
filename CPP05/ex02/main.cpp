e265c80a67f9110ff5cb6a24ddf13e04b20c4631
Jur Van toledo <jvan-tol@f0r2s19.codam.nl>
2023-02-02 15:42:50 +0100

    finished ex02

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -10,8 +10,8 @@ int main()
     std::cout << std::endl;
     try
     {
-        Bureaucrat jan("Jan", 25);
-        PresidentialPardonForm pres("De baas");
+        Bureaucrat jan("Jan", 4);
+        PresidentialPardonForm pres("De niet baas");
         std::cout << std::endl;
         jan.signForm(pres);
         jan.executeForm(pres);
@@ -29,7 +29,7 @@ int main()
     std::cout << std::endl;
     try
     {
-        Bureaucrat kees("Kees", 72);
+        Bureaucrat kees("Kees", 20);
         RobotomyRequestForm robot("cool");
         std::cout << std::endl;
         kees.signForm(robot);
@@ -50,8 +50,8 @@ int main()
     std::cout << std::endl;
     try
     {
-        Bureaucrat henk("Henk", 145);
-        ShrubberyCreationForm shrub("lol");
+        Bureaucrat henk("Henk", 40);
+        ShrubberyCreationForm shrub("boom");
         std::cout << std::endl;
         henk.signForm(shrub);
         henk.executeForm(shrub);
