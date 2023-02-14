5defec9d58c34a239eb8ea3796d427b40b26c8da
Jur Van toledo <jvan-tol@f1r5s19.codam.nl>
2023-02-14 14:30:06 +0100

    finished ex00

--- CPP07/ex00/include/whatever.hpp
+++ CPP07/ex00/include/whatever.hpp
@@ -6,9 +6,37 @@
     template <typename T>
     T swap(T val1, T val2)
     {
+        // T temp;
+
         val1 = val2;
-        return NULL;
+        // val2 = temp;
+        // temp = val1;
+        return 0;
     };
+
+    template <typename S>
+    S min(S val1, S val2)
+    {
+        if (val1 < val2)
+        {
+            std::cout << "The first value is called: " << val1 << std::endl;
+            return val1;
+        }
+        std::cout << "The second value is called: " << val2 << std::endl;
+        return val2;
+    }
+
+    template <typename M>
+    M max(M val1, M val2)
+    {
+        if (val1 > val2)
+        {
+            std::cout << "The first value is called: " << val1 << std::endl;
+            return val1;
+        }
+        std::cout << "The second value is called: " << val2 << std::endl;
+        return val2;
+    }
 
 
 #endif
--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+    int result1 = swap<int>(2, 3);
+    std::cout << "The first result: " << result1 << std::endl;
+
+    int result2 = min<int>(3, 2);
+    std::cout << "The second result: " << result2 << std::endl;
+
+    int result3 = max<int>(3, 2);
+    std::cout << "The third result: " << result3 << std::endl;
     return (0);
 }
