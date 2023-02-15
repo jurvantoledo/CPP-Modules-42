94c7b4bb72b445ed19294f4ee8d9f0a0f7c19f9d
Jur Van toledo <jvan-tol@f0r2s16.codam.nl>
2023-02-15 17:46:47 +0100

    added stuff

--- CPP08/ex01/include/Span.hpp
+++ CPP08/ex01/include/Span.hpp
@@ -1,14 +1,22 @@
 #ifndef SPAN_H
 
 #define SPAN_H
+#include <iostream>
 
 class Span
 {
     private:
-        /* data */
+        size_t max;
+
     public:
-        Span();
+        Span(size_t amount);
+        Span(const Span &obj);
+        Span &operator=(const Span &obj);
         ~Span();
+
+        void addNumber(int val);
+        size_t shortestSpan();
+        size_t longestSpan();
 };
 
 #endif
--- CPP08/ex01/src/Span.cpp
+++ CPP08/ex01/src/Span.cpp
@@ -1,9 +1,11 @@
 #include "../include/Span.hpp"
 
-Span::Span()
+Span::Span(size_t amount) : max(amount)
 {
+    std::cout << "Default constructor is called" << std::endl;
 }
 
 Span::~Span()
 {
+    std::cout << "Destructor is called" << std::endl;
 }
