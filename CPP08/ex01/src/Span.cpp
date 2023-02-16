8d27245595061f9deb6d100e81803860921bd6e6
Jur Van toledo <jvan-tol@f0r2s16.codam.nl>
2023-02-16 14:35:31 +0100

    added stuff

--- CPP08/ex01/src/Span.cpp
+++ CPP08/ex01/src/Span.cpp
@@ -5,6 +5,19 @@ Span::Span(size_t amount) : max(amount)
     std::cout << "Default constructor is called" << std::endl;
 }
 
+Span::Span(const Span &obj)
+{
+    std::cout << "Copy constructor is called" << std::endl;
+    *this = obj;
+}
+
+Span& Span::operator=(const Span &obj)
+{
+    std::cout << "Copy assignment operator is called" << std::endl;
+    (void)obj;
+    return (*this);
+}
+
 Span::~Span()
 {
     std::cout << "Destructor is called" << std::endl;
