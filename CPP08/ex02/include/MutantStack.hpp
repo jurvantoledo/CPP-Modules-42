34a10aa69507a0ebfa25ae6e1a7175add3736cac
Jur Van toledo <jvan-tol@f0r2s16.codam.nl>
2023-02-20 15:21:33 +0100

    added stuff

--- CPP08/ex02/include/MutantStack.hpp
+++ CPP08/ex02/include/MutantStack.hpp
@@ -1,5 +1,21 @@
 #ifndef MUTANTSTACK_H
 
 #define MUTANTSTACK_H
+#include <iostream>
+#include <exception>
+
+class MutantStack
+{
+    private:
+        
+    public:
+        MutantStack();
+        MutantStack(size_t n);
+        ~MutantStack();
+
+        MutantStack(const MutantStack &obj);
+        MutantStack& operator=(const MutantStack &obj);
+};
+
 
 #endif
