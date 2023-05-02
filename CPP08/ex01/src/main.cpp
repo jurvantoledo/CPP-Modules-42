6da4b79dfd35b07bb79869e57ae593a6eac89b99
Jur Van toledo <jvan-tol@f0r1s15.codam.nl>
2023-05-02 15:53:00 +0200

    fixed CPP08 -> ex01

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -2,56 +2,25 @@
 
 int main()
 {
-    {
-        Span s1 = Span(5);
+    Span s1 = Span(5);
 
-        try
-        {
-            s1.addNumber(5);
-            s1.addNumber(2);
-            s1.addNumber(3);
-            s1.addNumber(4);
-            s1.addNumber(1);
-            std::cout << "Shortes span: " << s1.shortestSpan() << std::endl;
-            std::cout << "Longest span: " << s1.longestSpan() << std::endl;    
-        }
-        catch(const Span::ArrayOverflow& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    }
+    try
     {
-        Span s1 = Span(5);
-        try
-        {
-            s1.addNumber(5);
-            s1.addNumber(2);
-            s1.addNumber(3);
-            s1.addNumber(4);
-            s1.addNumber(1);
-            s1.addNumber(6);
-            std::cout << "Shortes span: " << s1.shortestSpan() << std::endl;
-            std::cout << "Longest span: " << s1.longestSpan() << std::endl;    
-        }
-        catch(const Span::ArrayOverflow& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+        s1.addNumber(5);
+        s1.addNumber(2);
+        s1.addNumber(3);
+        s1.addNumber(4);
+        s1.addNumber(1);
+        std::cout << "Shortes span: " << s1.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << s1.longestSpan() << std::endl;    
     }
+    catch(const Span::ArrayOverflow& e)
     {
-        Span s1 = Span(5);
-        try
-        {
-            s1.addNumber(1);
-
-            std::cout << "Shortes span: " << s1.shortestSpan() << std::endl;
-            std::cout << "Longest span: " << s1.longestSpan() << std::endl;    
-        }
-        catch(const Span::NotEnoughNumbers& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+        std::cerr << e.what() << '\n';
     }
-    
+    catch (const Span::NotEnoughNumbers& e) 
+    {
+        std::cerr << e.what() << '\n';
+    }
     return (0);
 }
