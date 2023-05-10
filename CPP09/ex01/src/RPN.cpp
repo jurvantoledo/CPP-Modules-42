fb676828d5bc126750b2106e1094240d0616d098
Jur Van toledo <jvan-tol@f0r1s15.codam.nl>
2023-05-10 11:19:30 +0200

    finished CPP09 -> ex01

--- CPP09/ex01/src/RPN.cpp
+++ CPP09/ex01/src/RPN.cpp
@@ -2,22 +2,65 @@
 
 RPN::RPN(std::string input) : _data(input)
 {
-	std::cout << "Constructor is calles" << std::endl;
 }
 
 RPN::~RPN()
 {
-	std::cout << "Destructor is calles" << std::endl;
 }
 
 int RPN::CheckRPN()
 {
 	std::stack<int> stack;
+	int	res = 0;
 
 	for (size_t i = 0; i < _data.size(); i++)
 	{
-		std::cout << "The characters: " << _data[i] << std::endl;
+		if (isdigit(_data[i]))
+		{
+			if ((_data[i] >= '0' && _data[i] <= '9') && _data[i + 1] != ' ')
+			{
+				std::cout << "Number is too high!" << std::endl;
+				return (0);
+			}
+			int num = _data[i] - '0';
+			std::cout << num << std::endl;
+			stack.push(num);
+		}
+		else if (_data[i] == '*' || _data[i] == '-' || _data[i] == '+' || _data[i] == '/')
+		{
+			int y = stack.top();
+			stack.pop();
+			int x = stack.top();
+			stack.pop();
+
+			switch (_data[i])
+			{
+				case '*':
+					res = x * y;
+					break;
+				case '-':
+					res = x - y;
+					break;
+				case '+':
+					res = x + y;
+					break;
+				case '/':
+					res = x / y;
+					break;
+				default:
+					break;
+			}
+			std::cout << res << std::endl;
+			stack.push(res);
+		}
+		else
+		{
+			if (!isspace(_data[i]))
+			{
+				std::cout << "The math aint mathing bro" << std::endl;
+				return (0);
+			}
+		}
 	}
-	
-	return 0;
+	return stack.top() ;
 }
