0d9c54191926e53aaa9c97c986fc565876377f21
jurvantoledo <jurvantoledo@live.nl>
2023-05-11 12:11:47 +0200

    added some comments

--- CPP09/ex02/src/PmergeMe.cpp
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -21,11 +21,14 @@ void    PmergeMe::sort(std::vector<int> vec)
     for (size_t i = 0; i < vec.size(); i++)
 	{
 		size_t min_idx = i;
+		// std::cout << "min_idx: " << min_idx << std::endl;
 		for (size_t j = i + 1; j < vec.size(); j++)
 		{
+			// std::cout << "J: " << j << std::endl;
 			if (vec[j] < vec[min_idx])
 			{
 				min_idx = j;
+				// std::cout << "min_idx in if statement: " << min_idx << std::endl;
 			}
 		}
 		swap(vec[i], vec[min_idx]);
