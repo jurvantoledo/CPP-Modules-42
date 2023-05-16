5fe23c7939247a82c95dbbc9c63da90923b1bdb5
Jur Van toledo <jvan-tol@f1r6s10.codam.nl>
2023-05-16 15:17:59 +0200

    finished CPP09 -> ex02

--- CPP09/ex02/src/main.cpp
+++ CPP09/ex02/src/main.cpp
@@ -16,6 +16,7 @@ int	main(int argc, char *argv[])
 		return (0);
 	}
 
+	// Validating
 	gettimeofday(&start, NULL);
 	try
 	{
@@ -27,25 +28,67 @@ int	main(int argc, char *argv[])
 		std::cerr << e.what() << '\n';
 		return (EXIT_FAILURE);
 	}
-
-	pmerge.fillVector(vec, argv);
-	pmerge.fillDeque(deck, argv);
-	std::cout << "Given array is: " << std::endl;
-	std::cout << "Vector: "; 
-	pmerge.printVectorArray(vec);
-	std::cout << "Deque: ";
-	pmergeDeque.printDequeArray(deck);	
-
-	pmerge.mergeSort(vec, 0, vec.size() - 1);
-	pmergeDeque.mergeSortDeque(deck, 0, deck.size() - 1);
-
-	std::cout << "\nSorted array is" << std::endl;
-	std::cout << "Vector: "; 
-	pmerge.printVectorArray(vec);
-	std::cout << "Deque: ";
-	pmergeDeque.printDequeArray(deck);	
 	gettimeofday(&end, NULL);
 	diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	std::cout << "-------------------VALIDATING-----------------------" << std::endl;
+	std::cout << std::endl;
 	std::cout << validated << " arguments validated in " << diff << " μs" << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+	
+
+	// Pushing numbers in vector
+	std::cout << "-------------------ADD TO VECTOR--------------------" << std::endl;
+	std::cout << std::endl;
+	gettimeofday(&start, NULL);
+	pmerge.fillVector(vec, argv);
+	gettimeofday(&end, NULL);
+	diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	std::cout << "pushed all numbers to vector  in " << std::setfill(' ') << std::setw(9) << diff << " μs" << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+
+	std::cout << "-------------------GIVEN ARRAY----------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Given array is: ";
+	pmerge.printVectorArray(vec);
+	std::cout << std::endl;
+	std::cout << "-----------------------------------------------------" << std::endl;
+
+	// Merge sort vector
+	std::cout << "-------------------MERGE SORT VECTOR-----------------" << std::endl;
+	std::cout << std::endl;
+    gettimeofday(&start, NULL);
+	pmerge.mergeSort(vec, 0, vec.size() - 1);
+	gettimeofday(&end, NULL);
+	diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	std::cout << "sorted vector                 in " << std::setfill(' ') << std::setw(9) << diff << " μs" << std::endl;
+	pmerge.printVectorArray(vec);
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+	
+	//  Pushing numbers in vector
+	std::cout << "-------------------ADD TO DECK----------------------" << std::endl;
+	std::cout << std::endl;
+	gettimeofday(&start, NULL);
+	pmerge.fillDeque(deck, argv);
+	gettimeofday(&end, NULL);
+	diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	std::cout << "pushed all numbers to deck  in " << std::setfill(' ') << std::setw(9) << diff << " μs" << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+	
+	// Merge sort deque
+	std::cout << "-------------------MERGE SORT DECK-----------------" << std::endl;
+	std::cout << std::endl;
+    gettimeofday(&start, NULL);
+	pmergeDeque.mergeSortDeque(deck, 0, deck.size() - 1);
+	gettimeofday(&end, NULL);
+	diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	std::cout << "sorted deck                 in " << std::setfill(' ') << std::setw(9) << diff << " μs" << std::endl;
+	pmergeDeque.printDequeArray(deck);	
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+
 	return (0);
 }
