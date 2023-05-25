23212120446b90ef8fe34e2958d4bd817ffd78e4
Jur Van toledo <jvan-tol@f0r2s17.codam.nl>
2023-05-25 11:00:31 +0200

    Small fix CPP07 -> ex00

--- CPP07/ex00/include/whatever.hpp
+++ CPP07/ex00/include/whatever.hpp
@@ -14,22 +14,18 @@
     };
 
     template <typename T>
-    T min(T val1, T val2)
+    T min(T& val1, T& val2)
     {
         if (val1 < val2)
-        {
             return val1;
-        }
         return val2;
     }
 
     template <typename T>
-    T max(T val1, T val2)
+    T max(T& val1, T& val2)
     {
         if (val1 > val2)
-        {
             return val1;
-        }
         return val2;
     }
 
