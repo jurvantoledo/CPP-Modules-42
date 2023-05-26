e1e9bfb2583211e6649291b315244f83e993c89c
Jur Van toledo <jvan-tol@f0r2s17.codam.nl>
2023-05-26 11:04:52 +0200

    small fix CPP07 -> ex01

--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -6,11 +6,11 @@ int main()
     int arr[] = {0, 1, 2, 3, 4};
     int len = std::end(arr) - std::begin(arr);
 
-    ::iter(arr, len, &call);
+    iter(arr, len, &call);
 
 	char str[] = "lol lmao";
     std::cout << std::endl;
-    ::iter(str, strlen(str), &call);
+    iter(str, strlen(str), &call);
     std::cout << std::endl;
     
     return 0;
